3f0af5b8aefc6f56edf8f9f6411092fae09c86ae
ruxsorbek <ruhsorbeknorimmatov588@gmail.com>
2026-03-02 09:21:20 +0500

    don't wanna use i3wm for now

--- configuration.nix
+++ configuration.nix
@@ -1,95 +1,36 @@
-
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [ 
+    ./hardware-configuration.nix
+  ];
 
   # Enabling Flakes
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
-  # Bootloader.
+  # Bootloader & Kernel
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 10; # Keeps boot menu clean
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_zen;
 
-  # i3wm configuration
-  services = {
-    xserver = {
-      layout = "us";
-      xkbVariant = "";
-      enable = true;
-      windowManager.i3 = {
-        enable = true;
-        extraPackages = with pkgs; [
-          i3status
-        ];
-      };
-      desktopManager = {
-        xterm.enable = false;
-        xfce = {
-          enable = true;
-          noDesktop = true;
-          enableXfwm = false;
-        };
-      };
-      displayManager = {
-        lightdm.enable = true;
-        defaultSession = "xfce+i3";
-      };
-    };
-    gvfs.enable = true;
-    gnome.gnome-keyring.enable = true;
-    blueman.enable = true;
-    
-  };
+  # --- DESKTOP ENVIRONMENT (GNOME) ---
+  services.xserver.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
+  # Helper services for a smooth desktop experience
+  services.gnome.gnome-keyring.enable = true;
+  services.gvfs.enable = true;
+  services.blueman.enable = true;
 
-
-  # library for applying unpackaged programs
-  programs.nix-ld.enable = true;
-  programs.nix-ld.libraries = with pkgs;[
-  ];
-
-  # cfg for running AppImage Files
-  programs.appimage.enable = true;
-  programs.appimage.binfmt = true;
-
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  # Networking & Localization
+  networking.hostName = "nixos";
   networking.networkmanager.enable = true;
-
-  # Set your time zone.
   time.timeZone = "Asia/Tashkent";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
-  #Enable the redis server
-  services.redis.servers."default".enable = true;  
-
-
-  # Enable the GNOME Desktop Environment.
-  #services.displayManager.gdm.enable = true;
-  #services.desktopManager.gnome.enable = false;
-
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable PipeWire (audio)
+  # Hardware / Audio
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -97,97 +38,57 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    #jack.enable = true;  # Uncomment this if you need JACK support
   };
 
+  # Background Services
+  services.redis.servers."default".enable = true;
 
+  # Programs & Compatibility
+  programs.nix-ld.enable = true;
+  programs.appimage = {
+    enable = true;
+    binfmt = true;
+  };
+  programs.firefox.enable = true;
+  programs.steam.enable = true;
+  nixpkgs.config.allowUnfree = true;
 
-
-
-  # Enable touchpad support (enabled by default in most desktopManagers).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.ruxsorbek = {
     isNormalUser = true;
     description = "Ruxsorbek Norimmatov";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    ];
   };
 
-  # Enable automatic login for the user.
-  services.displayManager.autoLogin.enable = false;
-  services.displayManager.autoLogin.user = "ruxsorbek";
-
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
-
-  # Enable Firefox browser.
-  programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-
   environment.systemPackages = with pkgs; [
-    spotify
-    jre8
-    neovim
-    zed-editor
-    flatpak
+    # Applications
+    spotify 
     telegram-desktop
-    jetbrains.rust-rover
-    jetbrains.pycharm
     google-chrome
-    fastfetch
-    anydesk
-    python314
-    vim
-    wget
-    kitty
-    neofetch
-    git
     vscode
     obs-studio
-    gcc
-    ffmpeg
     vlc
+    # Development
+    neovim
+    zed-editor
+    jetbrains.rust-rover
+    jetbrains.pycharm
+    python314
+    gcc
+    git
     direnv
     nix-direnv
     wakatime-cli
+    jre8
+    # Utilities
+    fastfetch
+    neofetch
+    anydesk
+    vim
+    wget
+    kitty
+    flatpak
+    ffmpeg    
   ];
 
-  # Enable Steam (gaming)
-  programs.steam.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
+  system.stateVersion = "25.11";
 }
